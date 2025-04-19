4e7644ba3fdbe0e7e4891902b387c58d473f3b7d
DiarioDeObras2025 <relatoriodiariodeobras@gmail.com>
2025-04-19 01:20:11 -0300

    adiconado duplicacao de relatorio (#5)

--- DiarioObras/obj/Debug/net8.0/DiarioObras.AssemblyInfo.cs
+++ DiarioObras/obj/Debug/net8.0/DiarioObras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiarioObras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e81b978f37d1be36defb63abae404f91d05c7f93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6e97d55d582537c37ea94b8a67797db524f087")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiarioObras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiarioObras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
